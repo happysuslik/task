5a7247685b0753a1d4e7af04070bc60cfdeb3c32
Vyacheslav <happysuslikrealist@gmail.com>
2016-02-09 00:19:31 +0200

    Edit angular files controllers editTodoListCtrl and todoListCtrl angular.module

--- app/assets/javascripts/angular/controllers/editTodoListCtrl.js
+++ app/assets/javascripts/angular/controllers/editTodoListCtrl.js
@@ -1,24 +1,24 @@
 /**
  * Created by fod on 11.01.16.
  */
-app.controller("editTodoListCtrl", function($scope, Restangular, $stateParams, $state) {
-    $scope.task = Restangular.one('tasks', $stateParams.id).get().$object;
+angular.module("app")
+    .controller("editTodoListCtrl", function($scope, Restangular, $stateParams, $state) {
+        $scope.task = Restangular.one('tasks', $stateParams.id).get().$object;
 
-    $scope.save = function() {
-        Restangular.copy($scope.task).save();
-        $state.go('/home');
-    };
+        $scope.save = function() {
+            Restangular.copy($scope.task).save();
+            $state.go('/home');
+        };
 
-    $scope.cancel = function() {
-        $state.go('/home');
-    };
+        $scope.cancel = function() {
+            $state.go('/home');
+        };
 
-    $scope.getError = function(error) {
-        if (angular.isDefined(error)) {
-            if (error.required) {
-                return "Input no be blank"
+        $scope.getError = function(error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                    return "Input no be blank"
+                }
             }
-        }
-    };
-
-});
+        };
+    });
