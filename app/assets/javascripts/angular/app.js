6c102c53394643cca88933768ff8955b83028358
Vyacheslav <happysuslikrealist@gmail.com>
2016-02-09 12:05:25 +0200

    Editing js files controllers and removed 4 spaces

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,21 +1,22 @@
 var app = angular.module("app", ["ui.router", "restangular"]);
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $urlRouterProvider.otherwise('/home');
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+
+  $urlRouterProvider.otherwise('/home');
 
 
-    $stateProvider
-        .state('/home', {
-            url: '/home',
-            templateUrl: 'client_views/tasks.html',
-            controller: 'todoListCtrl'
-        })
-        .state('edit', {
-            url: '/:id/edit',
-            templateUrl: 'client_views/edit.html',
-            controller: 'editTodoListCtrl'
-        })
+  $stateProvider
+    .state('/home', {
+      url: '/home',
+      templateUrl: 'client_views/tasks.html',
+      controller: 'todoListCtrl'
+    })
+    .state('edit', {
+      url: '/:id/edit',
+      templateUrl: 'client_views/edit.html',
+      controller: 'editTodoListCtrl'
+    })
 });
--- app/assets/javascripts/angular/controllers/todoListCtrl.js
+++ app/assets/javascripts/angular/controllers/todoListCtrl.js
@@ -2,43 +2,42 @@
  * Created by fod on 02.01.16.
  */
 angular.module("app")
-    .controller("todoListCtrl", function($scope, Restangular, $state) {
-        $scope.refresh = function() {
-            Restangular.all('tasks').getList().then(function(tasks) {
-                $scope.tasks = tasks;
-            });
-        };
+  .controller("todoListCtrl", function($scope, Restangular, $state) {
+    $scope.refresh = function() {
+      Restangular.all('tasks').getList().then(function(tasks) {
+        $scope.tasks = tasks;
+      });
+    };
 
+    $scope.getError = function(error) {
+      if (angular.isDefined(error)) {
+        if (error.required) {
+          return "Input no be blank"
+        }
+      }
+    };
 
-        $scope.getError = function(error) {
-            if (angular.isDefined(error)) {
-                if (error.required) {
-                    return "Input no be blank"
-                }
-            }
-        };
+    $scope.addTask = function(task) {
+      $scope.task = {};
+      $scope.tasks.post(task).then(function(response) {
+        $scope.tasks.push(response);
+      });
+      $scope.task.text = '';
+      $scope.myForm.$setPristine();
+    };
 
-        $scope.addTask = function(task) {
-            $scope.task = {};
-            $scope.tasks.post(task).then(function(response) {
-                $scope.tasks.push(response);
-            });
-            $scope.task.text = '';
-            $scope.myForm.$setPristine();
-        };
+    $scope.destroy = function(task) {
+      Restangular.one('tasks', task.id).remove();
+      $scope.tasks.splice($scope.tasks.indexOf(task), 1);
+    };
 
-        $scope.destroy = function(task) {
-          Restangular.one('tasks', task.id).remove();
-          $scope.tasks.splice($scope.tasks.indexOf(task), 1);
-        };
+    $scope.editCheckBox = function(task) {
+      Restangular.copy(task).save();
+    };
 
-        $scope.editCheckBox = function(task) {
-            Restangular.copy(task).save();
-        };
+    $scope.edit = function(task) {
+      $state.go('edit', task);
+    };
 
-        $scope.edit = function(task) {
-            $state.go('edit', task);
-        };
-
-        $scope.refresh();
-    });
+    $scope.refresh();
+  });
