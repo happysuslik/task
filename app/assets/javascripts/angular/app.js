ac65cd87f03dc4f60881eef2a7a9fc70bb12e316
Vyacheslav <happysuslikrealist@gmail.com>
2016-02-09 14:37:55 +0200

    Add in app.js inject

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,21 +1,26 @@
 var app = angular.module("app", ["ui.router", "restangular"]);
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
 
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 
-  $stateProvider
-    .state('/home', {
-      url: '/home',
-      templateUrl: 'client_views/tasks.html',
-      controller: 'todoListCtrl'
-    })
-    .state('edit', {
-      url: '/:id/edit',
-      templateUrl: 'client_views/edit.html',
-      controller: 'editTodoListCtrl'
-    })
-});
+    $stateProvider
+      .state('/home', {
+        url: '/home',
+        templateUrl: 'client_views/tasks.html',
+        controller: 'todoListCtrl'
+      })
+      .state('edit', {
+        url: '/:id/edit',
+        templateUrl: 'client_views/edit.html',
+        controller: 'editTodoListCtrl'
+      })
+  }
+]);
