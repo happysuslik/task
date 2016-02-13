9f28dc946506467228c2a561e52907cd4d99feae
Vyacheslav <happysuslikrealist@gmail.com>
2016-02-13 18:27:27 +0200

    Add test code in sign_in_spec  visit admin

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'the sign in processing' do
   before(:each) do
     @user = create(:user)
+    @admin = create(:admin)
   end
 
   it 'sign out', js: true do
@@ -11,9 +12,10 @@ describe 'the sign in processing' do
     expect(page).to have_content 'Sign up'
   end
 
-  it 'sign me in', js: true do
+  it 'sign_in_user', js: true do
     sign_in_with(@user.email, @user.password)
     expect(page).to have_content 'All Tasks'
+    expect(page).to have_no_content 'Admin panel'
     click_button 'Sign out'
   end
 
@@ -21,6 +23,21 @@ describe 'the sign in processing' do
     sign_in_with(@user.email, @user.password)
     click_link 'Home'
     expect(page).to have_content 'All Tasks'
+  end
+
+  context 'visit admin' do
+    before do
+      sign_in_with(@admin.email, @admin.password)
+    end
+
+    it 'sign_in_admin', js: true do
+      expect(page).to have_content 'Admin panel'
+    end
+
+    it 'visit admin panel', js: true do
+      click_link 'Admin panel'
+      expect(page).to have_content 'Dashboard'
+    end
   end
 
 end
