9beea580a61b7acdcd86d2d86c2ad70511b44b2a
Vyacheslav <happysuslikrealist@gmail.com>
2016-02-15 00:55:30 +0200

    Create omniauth_callbacks_controller.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,6 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1036611456411290", "2391ac835ae537058dae6e7b1277218e"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   resources :tasks
   resources :client_views, only: [:show]
